c51d3eaacb95fb02f3acd6d9d82f2222639ec171
Arsenii Bylym <arseniibylym@gmail.com>
2019-05-10 10:08:51 +0300

    Show config file for heroku deploy

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    server: {
+        NODE_ENV: process.env.NODE_ENV || 'development',
+        PORT: process.env.PORT || '3000',
+        MONGO_DB_URI: process.env.MONGO_DB_URI || 'mongodb://localhost:27017/medium-app',
+        JWT_EXPIRATION_TIME: process.env.JWT_EXPIRATION_TIME || '1d',
+        JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'secret',
+        BASE_URI: process.env.BASE_URI  || 'http://localhost:3000',
+    },
+    client: {
+        JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || 'secret',
+        BASE_URI: process.env.BASE_URI  || 'http://localhost:3000',
+    }
+}
+
+module.exports = CONFIG;
